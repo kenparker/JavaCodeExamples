d700006537d58b4406555e52a8e0c636fcab4ac5
Angelo <mail@amaggioni.com>
2014-10-31 08:48:16 +0100

    HashMap Example added

--- Collections/src/main/java/com/maggioni/HashMap/HashMapStructure.java
+++ Collections/src/main/java/com/maggioni/HashMap/HashMapStructure.java
@@ -1,21 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.maggioni.HashMap;
+
+import java.util.HashMap;
+import java.util.Iterator;
 
 /**
  *
- * @author maggioni
+ * http://www.javacodegeeks.com/2014/03/how-hashmap-works-in-java.html
  */
 public class HashMapStructure {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+      
+        Country india = new Country("india", 1000);
+        Country japan = new Country("japan", 10000);
+        Country france = new Country("france", 2000);
+        Country russia = new Country("russia", 20000);
+        
+        HashMap<Country,String> countryCapitalMap = new HashMap<Country,String>();
+        countryCapitalMap.put(india, "Delhi");
+        System.out.println("hascode for : " + india.getName() + " is : "+ india.hashCode());
+        
+        countryCapitalMap.put(japan, "Tokyo");
+        System.out.println("hascode for : " + japan.getName() + " is : "+ japan.hashCode());
+        countryCapitalMap.put(france, "Paris");
+        countryCapitalMap.put(russia, "Moscow");
+        
+        Iterator<Country> countryCapitalIter = countryCapitalMap.keySet().iterator();
+        while (countryCapitalIter.hasNext()) {
+            Country countryObj = countryCapitalIter.next();
+            String capital = countryCapitalMap.get(countryObj);
+            System.out.println(countryObj.getName() + "-------------" + capital);
+        }
     }
     
 }
