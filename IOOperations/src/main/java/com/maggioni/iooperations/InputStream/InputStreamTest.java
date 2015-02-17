8054e0d60b941886f6d1e4ce48374c76d3acff6a
Angelo <mail@amaggioni.com>
2015-02-17 16:43:14 +0100

    InputStream Example changed

--- IOOperations/src/main/java/com/maggioni/iooperations/InputStream/InputStreamTest.java
+++ IOOperations/src/main/java/com/maggioni/iooperations/InputStream/InputStreamTest.java
@@ -1,4 +1,4 @@
-package com.maggioni.iooperations.Examples;
+package com.maggioni.iooperations.InputStream;
 
 import com.google.common.io.CharStreams;
 import java.io.BufferedInputStream;
@@ -22,26 +22,48 @@ public class InputStreamTest {
     public static void main(String[] args) throws UnsupportedEncodingException, IOException {
         
         FileInputStream file = new FileInputStream("InputFile.txt");
-        String stringFromStream = CharStreams.toString(new InputStreamReader(file, "UTF-8"));
+        String stringFromStream = CharStreams.toString(new InputStreamReader(file, "UTF-8")); // conversion to String
         System.out.println("String from InputFile.txt :" + stringFromStream);
         
+        System.out.println("\n");
         Reader fileReader = new FileReader("InputFile.txt");
-        stringFromStream = CharStreams.toString(fileReader);
+        stringFromStream = CharStreams.toString(fileReader); // conversion to String
         System.out.println("String from fileReader :" + stringFromStream);
-        
+        fileReader.close();
+              
         BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream("InputFile.txt"));
         stringFromStream = CharStreams.toString(new InputStreamReader(bufferedInputStream, "UTF-8"));
         System.out.println("String from BufferedInputStream :" + stringFromStream);
         
-       // BufferedReader br = new BufferedReader(bufferedInputStream);
+        System.out.println("");
+        InputStream file1 = new FileInputStream("InputFile.txt");
+        InputStream buffer1 = new BufferedInputStream(file1);
+        stringFromStream = CharStreams.toString(new InputStreamReader(buffer1));
+        System.out.println("String from BufferedInputStream :" + stringFromStream);
+        buffer1.close(); // important to close otherweise the next read will not work
+        
+        System.out.println("\n example using streams 1");
+        fileReader = new FileReader("InputFile.txt");
+        BufferedReader br = new BufferedReader(fileReader);
+        br.lines()
+                .forEach(l -> System.out.println(l));
+        br.close();
+        
+        System.out.println("\n example using streams 2");
+        file1 = new FileInputStream("InputFile.txt");
+        buffer1 = new BufferedInputStream(file1);
+        br = new BufferedReader(new InputStreamReader(buffer1));
+        br.lines()
+                .forEach(l -> System.out.println(l));
+        br.close();
         
         Reader initialReader = new StringReader("Firstline \n secondline \nthrird line");
         stringFromStream = CharStreams.toString(initialReader);
         System.out.println("String from StringReader :" + stringFromStream);
 
         // Conversion from String to InputStream
-        InputStream inputStream = new ByteArrayInputStream(stringFromStream.getBytes());
-        System.out.println(CharStreams.toString(new InputStreamReader(inputStream)));
+        InputStream inputStream = new ByteArrayInputStream(stringFromStream.getBytes()); // this is the actual conversion
+        System.out.println(CharStreams.toString(new InputStreamReader(inputStream))); // this line is just a test that the InputStream can be printed out
     }
     
 }
---
+++ IOOperations/src/main/java/com/maggioni/iooperations/InputStream/StreamsDemo.java
@@ -0,0 +1,25 @@
+package com.maggioni.iooperations.InputStream;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+/**
+ *
+ * @author magang
+ */
+public class StreamsDemo {
+    
+    public static void main(String[] args) throws FileNotFoundException {
+        InputStream file1 = new FileInputStream("InputFile.txt");
+        InputStream buffer1 = new BufferedInputStream(file1);
+        BufferedReader br = new BufferedReader(new InputStreamReader(buffer1));
+        br.lines()
+                .forEach(l -> System.out.println(l));
+    }
+}
