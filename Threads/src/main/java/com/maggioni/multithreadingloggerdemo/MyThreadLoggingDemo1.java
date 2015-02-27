869be88a4db8afe368d6425218e0e54d082c5b6f
Angelo <mail@amaggioni.com>
2015-02-27 13:58:50 +0100

    Threads mit Static Wrapper Logging Example added

---
+++ Threads/src/main/java/com/maggioni/multithreadingloggerdemo/MyThreadLoggingDemo1.java
@@ -0,0 +1,27 @@
+package com.maggioni.multithreadingloggerdemo;
+
+import com.jcabi.log.Logger;
+
+public class MyThreadLoggingDemo1 extends Thread{
+
+    public MyThreadLoggingDemo1 (String name) {
+        super(name);
+        Logger.info(this,"Logger initialized");
+    }
+    
+    @Override
+    public void run() {
+        Logger.info(this, "method #bar(%d) was called", 20);
+    }
+    
+    public static void main(String[] args) {
+       
+        
+        MyThreadLoggingDemo1 myThread;
+        for (int i = 0; i < 100; i++) {
+            myThread = new MyThreadLoggingDemo1("AngeloThread"+i);
+            myThread.start();
+        }
+    }
+    
+}
