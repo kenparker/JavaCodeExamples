bcf83976eab76a87cdd2517742815c9d62116e25
Angelo <mail@amaggioni.com>
2015-04-17 10:35:22 +0200

    More Examples added

---
+++ OCAExamples/src/main/java/com/maggioni/PartI/Chapter3/Exercise/Tailor.java
@@ -0,0 +1,13 @@
+package com.maggioni.PartI.Chapter3.Exercise;
+
+/**
+ *
+ * @author magang
+ */
+public class Tailor {
+    public static void main(String[] args) {
+        byte[][] ba = {{1,2,3,4},{1,2,3}};
+        System.out.println(ba[1].length + " " + ba.length);
+    }
+    
+}
