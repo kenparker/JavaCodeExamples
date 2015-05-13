f65d3bdea3bdd9f2a3bcba5bb7df869a4c7f10e9
angelo <magangde@yahoo.de>
2015-05-13 15:54:32 +0200

    CatDemo added

---
+++ OCAExamples/src/main/java/com/maggioni/Whizlab/Pretest/AnotherCatDemo1.java
@@ -0,0 +1,45 @@
+package com.maggioni.Whizlab.Pretest;
+
+class Animal {
+    public static void testClassMethod() {
+        System.out.println("The static method in Animal");
+    }
+    
+    public void testInstanceMethod() {
+        System.out.println("The instance Method in Animal");
+    }
+    
+    public Object testReturnObject(Object s) {
+        System.out.println("test method from Animal ");
+        return s;
+    }
+}
+public class AnotherCatDemo1 extends Animal{
+
+    public static void testClassMethod() {
+        System.out.println("The static method in Cat");
+    }
+    @Override
+    public void testInstanceMethod() {
+        System.out.println("The instance Method in Cat");
+    }
+    
+    @Override
+    public String testReturnObject(Object s) {
+        System.out.println("test method from Cat");
+        return "test";
+    }
+    public static void main(String[] args) {
+        AnotherCatDemo1 cat = new AnotherCatDemo1();
+        Animal animal = new Animal();
+        Animal.testClassMethod();
+        AnotherCatDemo1.testClassMethod();
+        cat.testInstanceMethod();
+        animal.testInstanceMethod();
+        
+        Animal newAnimal = cat;
+        newAnimal.testInstanceMethod();
+                
+    }
+
+}
