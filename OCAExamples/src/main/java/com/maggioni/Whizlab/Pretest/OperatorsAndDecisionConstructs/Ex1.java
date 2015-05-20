77d76decc8b1e4b7838cb5fb7a78468b6e6bec93
angelo <magangde@yahoo.de>
2015-05-20 08:41:25 +0200

    Operators and Decision Constructor tests added

---
+++ OCAExamples/src/main/java/com/maggioni/Whizlab/Pretest/OperatorsAndDecisionConstructs/Ex1.java
@@ -0,0 +1,19 @@
+package com.maggioni.Whizlab.Pretest.OperatorsAndDecisionConstructs;
+
+import sun.net.www.content.audio.x_aiff;
+
+public class Ex1 {
+
+    public static void main(String[] args) {
+        final int x;
+        x = 0;
+        final int y = 2;
+        
+        int i = (int)(Math.random()*4);
+        
+        switch(i) {
+            /*case x:{System.out.print("");} this statement do not compile because x is not a compile time constant*/
+        }
+    }
+
+}
---
+++ OCAExamples/src/main/java/com/maggioni/Whizlab/Pretest/OperatorsAndDecisionConstructs/Ex2.java
@@ -0,0 +1,39 @@
+package com.maggioni.Whizlab.Pretest.OperatorsAndDecisionConstructs;
+
+public class Ex2 {
+
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 5;
+        System.out.println("(j++ == 6 || i++ >=12) :"+(j++ == 6 || i++ >=12));
+        i = 10;
+        j = 5;
+        System.out.println("j++>=6 || (j++ == 6 || i++ >=12) :"+(j++>=6 || (j++ == 6 || i++ >=12)));      
+        System.out.println(i);  // 10
+        System.out.println(j);  // 7 
+        
+        i = 10;
+        j = 5;
+        System.out.println("(j++>=6 | (j++ == 6 | i++ >=12)) :"+(j++>=6 | (j++ == 6 | i++ >=12)));
+        System.out.println(i);  // 11
+        System.out.println(j);  // 7
+        
+        
+        
+        
+        j = 5;
+        System.out.println("j++ == 6 :" + (j++ == 6)); // false       
+        System.out.println(j);
+        
+        i = 10;
+        j = 5;
+        System.out.println((j++ == 6 || i++ >=12));
+        System.out.println(j);
+        System.out.println(i);
+        
+        if((i++ >=12)){ // false
+            System.out.println(i);
+        }
+    }
+
+}
