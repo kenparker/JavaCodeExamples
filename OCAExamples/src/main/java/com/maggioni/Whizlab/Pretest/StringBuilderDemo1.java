a376fbd168860a066b4c0dbd3a81df274fe06c18
Angelo <mail@amaggioni.com>
2015-06-19 14:42:07 +0200

    StringBuilder and String Demo added

---
+++ OCAExamples/src/main/java/com/maggioni/Whizlab/Pretest/StringBuilderDemo1.java
@@ -0,0 +1,19 @@
+package com.maggioni.Whizlab.Pretest;
+
+public class StringBuilderDemo1 {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Whizlabs");
+        char[] c = {' ', 'O', 'C', 'A', 'J'};
+        sb.append(c);
+        System.out.println(sb);
+        sb.replace(0, 4, "OCAJP");
+        System.out.println(sb);
+        System.out.println(sb.length());
+        sb.setCharAt(3, 'W');
+        System.out.println(sb);
+
+        char[] charArray = {' ', 'O', 'C', 'A', 'J'};
+    }
+
+}
