5a0ca2f7a0c82c9b4db863a139e12557020713cb
angelo <magangde@yahoo.de>
2015-10-29 15:52:11 +0100

    enum Demos added

---
+++ OCPExamples/src/main/java/com/maggioni/AdvancedClassDesign/EnumDemo1.java
@@ -0,0 +1,24 @@
+package com.maggioni.AdvancedClassDesign;
+
+import   com.maggioni.AdvancedClassDesign.Better.Faster;
+import static com.maggioni.AdvancedClassDesign.Better.Faster.Longer;
+public class EnumDemo1 {
+
+    public static void main(String[] args) {
+        Better.Faster b = Better.Faster.Higher; // It do not need an import
+        Faster c = Longer; // It works because of the Static import
+        
+        Better.Faster b2;
+        b2 = Better.Faster.Higher;
+    }
+
+}
+
+class Better {
+    enum Faster {Higher, Longer};
+    
+    void doS() {
+        Faster f = Faster.Higher;
+        Faster g = Longer;
+    }
+}
---
+++ OCPExamples/src/main/java/com/maggioni/AdvancedClassDesign/EnumDemo2.java
@@ -0,0 +1,34 @@
+package com.maggioni.AdvancedClassDesign;
+
+/*
+    OCP Objective 2.5
+    Page 60
+*/
+public class EnumDemo2 {
+
+    enum Roman {I, V, X, L, C, M;
+    
+    }
+    public static void main(String[] args) {
+        int x = 7;
+        int z = 2;
+        Roman r = Roman.X;
+        Roman[] values = Roman.values();
+        for (Roman value : values) {
+            System.out.println(value);
+        }
+        do {            
+            
+            switch (r) {
+                case C: r = Roman.L; break;
+                case X: r = Roman.C;
+                case L: System.out.println("ordinal "+r.ordinal()); if (r.ordinal() > 2) z+= 5;
+                case M: x++;
+            }
+            z++;
+            System.out.println("x = "+ x + "  z =" +z);
+        } while (x < 10);
+        System.out.println(z);
+    }
+
+}
