a9ee1a3680fc3ac0bfdf8260ac31a5201cd28c1c
angelo <magangde@yahoo.de>
2015-11-12 13:23:36 +0100

    Generic Demo enhanced

--- OCPExamples/src/main/java/com/maggioni/CollectionsAndGenerics/GenericDemo1.java
+++ OCPExamples/src/main/java/com/maggioni/CollectionsAndGenerics/GenericDemo1.java
@@ -1,25 +1,52 @@
 package com.maggioni.CollectionsAndGenerics;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
+/*
+Chapter 11
+https://docs.oracle.com/javase/tutorial/extra/generics/methods.html
+http://openbook.rheinwerk-verlag.de/javainsel/javainsel_09_005.html#dodtp3d148a00-5e2e-421f-8bae-99afc9bf304a
+*/
 public class GenericDemo1 {
 
     public static void main(String[] args) {
         
-        List<Animal> li = new ArrayList<>();
-        li.add(new Animal());
-        li.add(new Fish());
-        li.add(new Siamesi());
-        doFoo1(li);
-        doFoo2(li);
-        doFoo3(li);
-        doFoo4(li);
+        List<Animal> animals = new ArrayList<>();
+        animals.add(new Animal());
+        animals.add(new Fish());
+        animals.add(new Siamesi());
+        doFoo1(animals);
+        doFoo2(animals);
+        doFoo3(animals);
+        doFoo4(animals);
         //doFoo5(li); do not compile Animal cannot be converted to Cat
         List<Siamesi> siamesis = new ArrayList<>();
         siamesis.add(new Siamesi());
-        //doFoo5(siamesis); do not compile Siamesi cannot be converted to Cat
+        doFoo5(siamesis); // ok no problems
+        List<Cat> cats = new ArrayList<>(Arrays.asList(new Cat(),new Cat()));
+        doFoo5(cats); /// ok no problems
+        doFoo6(cats); /// ok no problems
+        doFoo7(cats); /// ok no problems
+        doFoo7(animals); /// ok no problems
+        
+        // more list declarations
+        List<?> fishList = new ArrayList<Fish>();
+        //fishList.add(new Fish()); do not work
+        Object getObject = fishList.get(0); // works return type is always an object
+        
+        List<? extends Animal> animals1 = new ArrayList<Animal>();
+        //animals1.add(new Fish()); do not compile
+        Animal get = animals1.get(0);
+        
+        List<? super Cat> siamesiList = new ArrayList<Cat>();
+        siamesiList.add(new Siamesi());
+        siamesiList.add(new Cat());
+        Object get1 = siamesiList.get(0);
+        Cat get2 = (Cat) siamesiList.get(0);
+        
     }
 
     public static List doFoo1(List li) {
@@ -36,8 +63,20 @@ public class GenericDemo1 {
     public static List doFoo4(List<Animal> a) {
         return new ArrayList();
     }
-    public static List doFoo5(List<Cat> a) {
+    public static List doFoo5(List<? extends Cat> a) {
         return new ArrayList();
+    }
+    public static List doFoo6(List<? super Cat> a) {       
+        a.add(new Cat());       // works no problem
+        a.add(new Siamesi());   // works no problem
+        //a.add(new Animal()); // do not work as Animal ist in the hierarchy lower then Cat
+        return a;
+    }
+    public static List doFoo7(List<?> a) {       
+        //a.add(new Cat());       // do not work
+        //a.add(new Siamesi());   // you cannot add anything
+        //a.add(new Animal()); // do not work as Animal ist in the hierarchy lower then Cat
+        return a;
     }
     
 }
