ba4c3b98022a73dce56688013adbb3e3c8537f0d
angelo <magangde@yahoo.de>
2016-01-13 16:08:44 +0100

    Assertions Demos

---
+++ OCPExamples/src/main/java/com/maggioni/Assertions/ThrowDetailedAssertionErrorDemo1.java
@@ -0,0 +1,10 @@
+package com.maggioni.Assertions;
+
+public class ThrowDetailedAssertionErrorDemo1 {
+
+    public static void main(String[] args) {
+
+        assert false: "Testing assertions";
+    }
+
+}
