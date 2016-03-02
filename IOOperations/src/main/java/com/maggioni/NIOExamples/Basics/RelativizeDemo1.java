09beb27981f2617c0de515e5c19aec601249d44a
angelo <magangde@yahoo.de>
2016-03-02 15:21:38 +0100

    Relativize and Resolving Demo extended

--- IOOperations/src/main/java/com/maggioni/NIOExamples/Basics/RelativizeDemo1.java
+++ IOOperations/src/main/java/com/maggioni/NIOExamples/Basics/RelativizeDemo1.java
@@ -3,6 +3,37 @@ package com.maggioni.NIOExamples.Basics;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+/*
+Creating a Path Between Two Paths
+https://docs.oracle.com/javase/tutorial/essential/io/pathOps.html#relativize
+
+A common requirement when you are writing file I/O code is the capability to construct a path from one location in the file system to another location. 
+You can meet this using the relativize method. 
+This method constructs a path originating from the original path and ending at the location specified by the passed-in path. The new path is relative to the original path.
+
+For example, consider two relative paths defined as joe and sally:
+
+Path p1 = Paths.get("joe");
+Path p2 = Paths.get("sally");
+In the absence of any other information, it is assumed that joe and sally are siblings, meaning nodes that reside at the same level in the tree structure. To navigate from joe to sally, you would expect to first navigate one level up to the parent node and then down to sally:
+
+// Result is ../sally
+Path p1_to_p2 = p1.relativize(p2);
+// Result is ../joe
+Path p2_to_p1 = p2.relativize(p1);
+Consider a slightly more complicated example:
+
+Path p1 = Paths.get("home");
+Path p3 = Paths.get("home/sally/bar");
+// Result is sally/bar
+Path p1_to_p3 = p1.relativize(p3);
+// Result is ../..
+Path p3_to_p1 = p3.relativize(p1);
+In this example, the two paths share the same node, home. To navigate from home to bar, you first navigate one level down to sally and then one more level down to bar. Navigating from bar to home requires moving up two levels.
+
+A relative path cannot be constructed if only one of the paths includes a root element. If both paths include a root element, the capability to construct a relative path is system dependent.
+*/
+
 public class RelativizeDemo1 {
 
     public static void main(String[] args) {
--- IOOperations/src/main/java/com/maggioni/NIOExamples/Basics/ResolvingPathDemo1.java
+++ IOOperations/src/main/java/com/maggioni/NIOExamples/Basics/ResolvingPathDemo1.java
@@ -3,40 +3,74 @@ package com.maggioni.NIOExamples.Basics;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+/*
+ Joining Two Paths
+https://docs.oracle.com/javase/tutorial/essential/io/pathOps.html#resolve
+
+ You can combine paths by using the resolve method. 
+ You pass in a partial path , which is a path that does not include a root element, and that partial path is appended to the original path.
+ For example, consider the following code snippet:
+
+ // Solaris
+ Path p1 = Paths.get("/home/joe/foo");
+ // Result is /home/joe/foo/bar
+ System.out.format("%s%n", p1.resolve("bar"));
+
+ or
+
+ // Microsoft Windows
+ Path p1 = Paths.get("C:\\home\\joe\\foo");
+ // Result is C:\home\joe\foo\bar
+ System.out.format("%s%n", p1.resolve("bar"));
+
+ Passing an absolute path to the resolve method returns the passed-in path:
+ // Result is /home/joe
+ Paths.get("foo").resolve("/home/joe");
+ */
 public class ResolvingPathDemo1 {
 
     public static void main(String[] args) {
         Path absolute = Paths.get("/home/java");
         Path relative = Paths.get("dir");
         Path file = Paths.get("model.pdf");
-        
+
         System.out.println("1 : " + absolute.resolve(relative));
+        // --> 1 : \home\java\dir
         System.out.println("2 : " + absolute.resolve(file));
+        // --> 2 : \home\java\model.pdf
         System.out.println("3 : " + relative.resolve(file));
+        // --> 3 : dir\model.pdf
         System.out.println("4 : " + relative.resolve(absolute)); // bad
+        // --> 4 : \home\java
         System.out.println("5 : " + file.resolve(absolute));    // bad
+        // --> 5 : \home\java
         System.out.println("6 : " + file.resolve(relative));    // bad
-        
+        // --> 6 : model.pdf\dir
+
         /*
-        String pathNull1 = null;
-        System.out.println("7 : " + absolute.resolve(pathNull1)); // compile but will throw "NullPointerException
-        System.out.println("8 : " + absolute.resolve((String) pathNull1)); // compile but will throw "NullPointerException
-        System.out.println("9 : " + absolute.resolve((String) null)); // compile but will throw "NullPointerException*/
-        
+         String pathNull1 = null;
+         System.out.println("7 : " + absolute.resolve(pathNull1)); // compile but will throw "NullPointerException
+         System.out.println("8 : " + absolute.resolve((String) pathNull1)); // compile but will throw "NullPointerException
+         System.out.println("9 : " + absolute.resolve((String) null)); // compile but will throw "NullPointerException*/
         String pathNull2;
         //System.out.println("7 : " + absolute.resolve(pathNull2)); // do not compile because var = null
-        
+
         absolute = Paths.get("c:/");
         relative = Paths.get(".");
         System.out.println("10 : " + absolute.resolve(relative));
+        // --> 10 : c:\.
         System.out.println("11 : " + relative.resolve(absolute));
-     
-        
-        
+        // --> 11 : c:\
+
         Path p1 = Paths.get("\\personal\\readme.txt");
         Path p2 = Paths.get("\\index.txt");
         Path p3 = p1.resolve(p2);
-        System.out.println("12: "+p3.toString());
+        System.out.println("12: " + p3.toString());
+        // --> 12: \index.txt
+
+        Path p4 = Paths.get("C:\\home\\joe\\foo");
+        // Result is C:\home\joe\foo\bar
+        System.out.format("13: %s%n", p4.resolve("bar"));
     }
 
 }
