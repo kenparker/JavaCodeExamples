d3d2f87ef6c0a16ee189e9955171eeb58b168c7d
Angelo <mail@amaggioni.com>
2016-03-04 15:21:26 +0100

    Generic Question Real Brainer added

---
+++ OCPExamples/src/main/java/com/maggioni/CollectionsAndGenerics/MostDifficults/GenericQuestionRealBrainer.java
@@ -0,0 +1,46 @@
+package com.maggioni.CollectionsAndGenerics.MostDifficults;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericQuestionRealBrainer {
+
+    public static void main(String[] args) {
+        option1();
+    }
+
+    private static void option1() {
+        ArrayList<String> in = new ArrayList();
+        List result;
+        result = doIt(in);
+    }
+    private static void option2() {
+        List<CharSequence> in = new ArrayList();
+        List result;
+        result = doIt(in);
+    }
+    private static void option3() {
+        List<CharSequence> in = new ArrayList();
+        List<CharSequence> result;
+        //result = doIt(in); // do not compile
+    }
+    private static void option4() {
+        List<CharSequence> in = new ArrayList();
+        List<? super CharSequence> result;
+        result = doIt(in);
+    }
+    private static void option41() {
+        List<CharSequence> in = new ArrayList();
+        List<? super String> result;
+        result = doIt(in);
+    }
+    private static void option5() {
+        List<CharSequence> in = new ArrayList();
+        List<Object> result;
+        //result = doIt(in); // do not compile
+    }
+
+    public static <E extends CharSequence> List<? super E> doIt(List<E> nums){
+        return nums;
+    }
+}
