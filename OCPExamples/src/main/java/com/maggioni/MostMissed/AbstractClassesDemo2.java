49174d97bcf4ecdb407b1c8f9abe9d5a48373d1c
angelo <magangde@yahoo.de>
2016-05-03 14:59:55 +0200

    Abstract Class Demo added

---
+++ OCPExamples/src/main/java/com/maggioni/MostMissed/AbstractClassesDemo2.java
@@ -0,0 +1,13 @@
+package com.maggioni.MostMissed;
+
+abstract class AbstractClassesDemo2 {
+
+    final void f() {}
+    // abstract synchronized void g(); // abstract and synchronized together not allowed
+    static  void h() {}
+    public static void main(String[] args) {
+        AbstractClassesDemo2 obj;
+        System.out.println("Hello");
+    }
+
+}
