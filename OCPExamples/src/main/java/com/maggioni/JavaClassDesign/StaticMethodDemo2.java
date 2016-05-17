f3efd88d058b37cc11d8085372151ef378d8cb54
angelo <magangde@yahoo.de>
2016-05-17 12:43:20 +0200

    Existing Demo modified

--- OCPExamples/src/main/java/com/maggioni/JavaClassDesign/StaticMethodDemo2.java
+++ OCPExamples/src/main/java/com/maggioni/JavaClassDesign/StaticMethodDemo2.java
@@ -15,8 +15,8 @@ public class StaticMethodDemo2 {
     public static void main(String[] args) {
         Car2 c = new Mini2();
         c.m1();                     // polymorphism applies ONLY to instance methods
-        System.out.print(" "+c.i1+c.i2); // NOT to static methods and NOT to static AND instance variables
-        c.m2();     // from Mini2
+        System.out.print(" "+c.i1+c.i2); // NOT to static methods and NOT to static AND instance variables -> Print -> 56
+        c.m2();     // from Mini2 -> print -> 8
         
         System.out.println("");
         Mini2 m = new Mini2();
@@ -30,8 +30,8 @@ public class StaticMethodDemo2 {
 class Car2 {
     static int i1 = 5;
     int i2 = 6;
-    public static void m1() {System.out.print(i1);}
-    public void m1(String s) {} // valid
+    public static void m1() {System.out.println("from static"); System.out.print(i1);}  // print -> 5
+    public void m1(String s) {System.out.println("from instance");} // valid
     public void m2() {System.out.println(i2);}
 }
 
@@ -40,5 +40,5 @@ class Mini2 extends Car2 {
     int i2 = 8;
     public static void m1() {System.out.print(i1);}
     @Override
-    public void m2() {System.out.println(i2);}
+    public void m2() {System.out.println(i2);}  
 }
