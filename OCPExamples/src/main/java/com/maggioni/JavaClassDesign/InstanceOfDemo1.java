ef68ed19e2bbdce0cb5f870234bd07bec798c82d
angelo <magangde@yahoo.de>
2016-08-03 10:04:19 +0200

    InstanceOf Demo checked

--- OCPExamples/src/main/java/com/maggioni/JavaClassDesign/InstanceOfDemo1.java
+++ OCPExamples/src/main/java/com/maggioni/JavaClassDesign/InstanceOfDemo1.java
@@ -5,9 +5,26 @@ public class InstanceOfDemo1 {
     public static void main(String[] args) {
         Short s = 15;
         Boolean b;
-        //b = (s instanceof String); do not compile
+        
+        //b = (s instanceof String); //do not compile
         b = (s instanceof Number);
         b = (s instanceof Short);
+        
+        Alpha alpha = new Gamma();
+        Gamma gamma = new Gamma();
+        Foo foo = new Foo();
+        
+        Alpha a1 = (Alpha)foo; // this works only because Alpha is an interface
+                                // if Alpha were a Class, then the statement do not compile
+        
+        if (alpha instanceof Beta) { }
+        if (foo instanceof Alpha) { }
+               
     }
 
 }
+
+interface Alpha{}
+class Beta implements Alpha{}
+class Gamma extends Beta{}
+class Foo{}
