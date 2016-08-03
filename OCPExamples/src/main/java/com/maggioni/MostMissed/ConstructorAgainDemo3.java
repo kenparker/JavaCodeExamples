e1bdcd7866203981f3d4156b23f2a58f1d9ff0f4
angelo <magangde@yahoo.de>
2016-08-03 10:05:12 +0200

    Constructor Demos

---
+++ OCPExamples/src/main/java/com/maggioni/MostMissed/ConstructorAgainDemo3.java
@@ -0,0 +1,11 @@
+package com.maggioni.MostMissed;
+
+import com.maggioni.MostMissed.PracticeTestII.ConstructorAgainDemo2;
+
+public class ConstructorAgainDemo3 {
+
+    public static void main(String[] args) {
+        //ConstructorAgainDemo2 w = new ConstructorAgainDemo2(); // compilation error as constructor is protected
+    }
+
+}
---
+++ OCPExamples/src/main/java/com/maggioni/MostMissed/ConstructorAgainDemo5.java
@@ -0,0 +1,12 @@
+package com.maggioni.MostMissed;
+
+import com.maggioni.MostMissed.PracticeTestII.ConstructorAgainDemo2;
+
+public class ConstructorAgainDemo5 extends ConstructorAgainDemo2{
+
+    public static void main(String[] args) {
+        //ConstructorAgainDemo2 c2 = new ConstructorAgainDemo2();   // do not compile
+        ConstructorAgainDemo2 c2 = new ConstructorAgainDemo5();   // do not compile
+    }
+
+}
