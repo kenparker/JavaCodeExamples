c96569c42df13bde46a8cf5cc17aaf5daaeac65c
angelo <magangde@yahoo.de>
2018-02-22 14:48:26 +0100

    Bookmarks added

--- Java8Examples/src/test/java/com/maggioni/Function/FunctionComposeDemo2.java
+++ Java8Examples/src/test/java/com/maggioni/Function/FunctionComposeDemo2.java
@@ -8,6 +8,15 @@ public class FunctionComposeDemo2 {
     // https://howtodoinjava.com/java-8/how-to-use-predicate-in-java-8/
     // http://winterbe.com/posts/2014/03/16/java-8-tutorial/
     // http://www.baeldung.com/java-8-comparator-comparing
+    /*
+    http://www.angelikalanger.com/Articles/EffectiveJava/70.Java8.FunctionalProg/70.Java8.FunctionalProg.html
+    http://www.deadcoderising.com/2015-09-07-java-8-functional-composition-using-compose-and-andthen/
+    http://www.baeldung.com/java-8-sort-lambda
+    http://www.baeldung.com/guice
+    https://jaxenter.com/enumset-in-java-regularenumset-vs-jumboenumset-106051.html
+
+
+     */
 
 
 }
