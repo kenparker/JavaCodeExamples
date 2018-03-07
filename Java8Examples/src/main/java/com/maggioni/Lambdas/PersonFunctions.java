86fd871e14d85fcf18a861dafb4395fd2273b602
angelo <magangde@yahoo.de>
2018-03-07 14:32:45 +0100

    Lambda Example Person

--- Java8Examples/src/main/java/com/maggioni/Lambdas/PersonFunctions.java
+++ Java8Examples/src/main/java/com/maggioni/Lambdas/PersonFunctions.java
@@ -3,14 +3,22 @@ package com.maggioni.Lambdas;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class PersonFunctions {
 
-    Predicate<Integer> ageOver65 = age -> age > 65;
+    static Predicate<Integer> ageOver65 = age -> age > 65;
 
+    static Function<List<? extends PersonInterface>, Stream<Integer>> streamAges =(List<? extends PersonInterface> persons) -> persons.stream()
+                                                                                                             .map(PersonInterface::getAge);
 
-    Function<List<PersonInterface>, Boolean> filterPersonsOver65 = (List<PersonInterface> persons) -> persons.stream()
-                                                                            .map(PersonInterface::getAge)
-                                                                            .anyMatch(ageOver65);
+    static Function<Stream<Integer>, Boolean> isAgesOver65 = stream -> stream.anyMatch(ageOver65);
+    static Function<Stream<Integer>, List<Integer>> allElementsToList = stream -> stream.collect(Collectors.toList());
+
+   static Function<List<? extends PersonInterface>, Boolean> sss = persons -> {
+       Stream<Integer> integerStream = persons.stream().map(PersonInterface::getAge);
+       return integerStream.anyMatch(ageOver65);
+   };
 
 }
--- Java8Examples/src/test/java/com/maggioni/Lambdas/PersonTest.java
+++ Java8Examples/src/test/java/com/maggioni/Lambdas/PersonTest.java
@@ -4,7 +4,12 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static com.maggioni.Lambdas.PersonInterface.*;
 import static org.junit.Assert.*;
@@ -32,5 +37,20 @@ public class PersonTest {
         name = "Angelo";
         gender = Gender.MALE;
         person = new Person.Builder().age(age).name(name).gender(gender).build();
+    }
+
+    @Test
+    public void testStreamOfAges() {
+        List<Person> persons = new ArrayList<>();
+        persons.add(new Person.Builder().age(10).name("Angelo").gender(Gender.MALE).build());
+        persons.add(new Person.Builder().age(66).name("Marco").gender(Gender.MALE).build());
+
+        Stream<Integer> stream = PersonFunctions.streamAges.apply(persons);
+        //List<Integer> integers = stream.collect(Collectors.toList());
+        //integers.forEach(System.out::println);
+
+        assertTrue(stream.allMatch(a -> a == 10 || a == 66));
+
+
     }
 }
