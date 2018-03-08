25e635dfc761c07669ef79723091338bf14dd0e3
angelo <magangde@yahoo.de>
2018-03-08 14:33:48 +0100

    Subclassing Builder Pattern moved to new Project

--- DesignPattern/src/test/java/SubclassingBuilder/GMDFactsTest.java
+++ DesignPattern/src/test/java/SubclassingBuilder/GMDFactsTest.java
@@ -1,8 +1,7 @@
-package com.maggioni.SubclassingBuilder;
+package SubclassingBuilder;
 
+import com.maggioni.SubclassingBuilder.GMDFacts;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 public class GMDFactsTest {
 
