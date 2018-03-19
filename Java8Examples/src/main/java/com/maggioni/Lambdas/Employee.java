088c69914ee38c2123aecab7e8c6b5304634975a
angelo <magangde@yahoo.de>
2018-03-19 12:51:35 +0100

    Builder Pattern articles

--- Java8Examples/src/main/java/com/maggioni/Lambdas/Employee.java
+++ Java8Examples/src/main/java/com/maggioni/Lambdas/Employee.java
@@ -1,7 +1,10 @@
 package com.maggioni.Lambdas;
 
 import java.util.Optional;
-
+/*
+https://stackoverflow.com/questions/17164375/subclassing-a-java-builder-class
+https://metabroadcast.com/blog/builder-pattern-and-inheritance
+ */
 public class Employee extends Person {
 
     private Integer salary;
