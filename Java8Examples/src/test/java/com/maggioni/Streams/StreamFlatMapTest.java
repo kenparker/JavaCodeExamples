6965f4a057177c1842faf51fb090dad7047f89f8
Angelo <Angelo@DESKTOP-PQRPD3V.fritz.box>
2018-04-13 14:58:02 +0200

    Map and FlatMap examples added

--- Java8Examples/src/test/java/com/maggioni/Streams/StreamFlatMapTest.java
+++ Java8Examples/src/test/java/com/maggioni/Streams/StreamFlatMapTest.java
@@ -5,6 +5,7 @@ import com.maggioni.Lambdas.Department;
 import com.maggioni.Lambdas.Employee;
 import org.junit.Test;
 
+import java.util.Set;
 import java.util.stream.Stream;
 
 public class StreamFlatMapTest extends CorporationBuilderTest {
@@ -19,8 +20,49 @@ public class StreamFlatMapTest extends CorporationBuilderTest {
         corporation.getDepartments().stream()
                 .flatMap(department -> {
                     System.out.println("department : " + department.getName());
-                    return department.getEmployees().stream();})
+                    return department.getEmployees().stream();
+                })
                 .forEach(System.out::println);
+    }
+
+    @Test
+    public void testStreamAndStreamOf() {
+        Set<Department> departments = corporation.getDepartments();
+
+        Stream<Employee> employeeStream = departments.stream()
+                .flatMap(department -> department.getEmployees().stream());
+
+        Stream<Set<Employee>> setStream = departments.stream()
+                .flatMap(department -> Stream.of(department.getEmployees()));
+        Stream<Set<Employee>> setStream1 = departments.stream()
+                .map(department -> department.getEmployees());
+
+    }
+
+    @Test
+    public void testFlatMapCorporation1() {
+        corporation.getDepartments().stream()
+                .flatMap(department -> {
+                    System.out.println("department : " + department.getName());
+                    return Stream.of(department.getEmployees());
+                })
+                .flatMap(employee -> {
+                    return employee.stream();
+                })
+                .forEach(System.out::println);
+    }
+
+    @Test
+    public void testFlatMapCorporation2() {
+        corporation.getDepartments().stream()
+                .flatMap(department -> {
+                    System.out.println("department :  " + department.getName());
+                    return department.getEmployees().stream()
+                            .flatMap(employee -> Stream.of(employee));
+                })
+                .forEach(System.out::println);
+
+
     }
 
     @Test
