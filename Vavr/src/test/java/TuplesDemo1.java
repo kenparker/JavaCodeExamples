4543409fe74e66b99a6fa1efee54567b81001a6d
Angelo <Angelo@DESKTOP-PQRPD3V.fritz.box>
2018-04-20 13:18:38 +0200

    TuplesDemo extended

--- Vavr/src/test/java/TuplesDemo1.java
+++ Vavr/src/test/java/TuplesDemo1.java
@@ -4,10 +4,14 @@ import com.maggioni.Lambdas.Person;
 import com.maggioni.Lambdas.PersonInterface;
 import io.vavr.Tuple;
 import io.vavr.Tuple2;
+import io.vavr.Tuple3;
 import io.vavr.Tuple4;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 import static org.junit.Assert.*;
 
@@ -37,5 +41,52 @@ public class TuplesDemo1 {
     public void tupleOfFour() {
         Tuple4<Person, Employee, Person, Employee> tupleOfFourElements = Tuple.of(personAngelo, employeeAngelo, personMarco, employeeNancy);
         assertEquals(personAngelo,tupleOfFourElements._1);
+    }
+
+    @Test
+    public void tupleMapBifunction() {
+        Tuple2<Employee,Employee> tupleMap = Tuple.of(employeeAngelo,employeeNancy);
+
+        BiFunction<? super Employee, ? super Employee, Tuple2<Employee, Employee>> bifunction = ( e1, e2) -> Tuple.of(e2,e1);
+        Tuple2<Employee, Employee> that2 = tupleMap.map(bifunction);
+        assertEquals(employeeAngelo,that2._2);
+
+        Tuple2<Employee, Employee> that1 = tupleMap.map(( e1, e2) -> Tuple.of(e2,e1));
+        assertEquals(employeeNancy,that1._1);
+    }
+
+    @Test
+    public void tupleMapFunction1() {
+        Tuple3<Employee, Person, String> tuple3 = Tuple.of(employeeNancy, personAngelo, "The String is : ");
+
+        Tuple3<Employee, Person, String> map = tuple3.map(employee -> employee, person -> person, s -> s.concat(employeeAngelo.getName().get()
+                                                                                                        .concat(" e ")
+                                                                                                        .concat(personMarco.getName().get())));
+        System.out.println(map._3);
+    }
+
+    @Test
+    public void tupleMapFunction2() {
+        int expected = employeeAngelo.getSalary() + 2000;
+        Tuple2<Employee, Person> tuple2Before = Tuple.of(employeeAngelo, personMarco);
+
+        Function<Employee, Employee> fEmployee  = employee -> new Employee.EmployeeBuilder()
+                                                                .name(employee.getName().get())
+                                                                .salary(expected)
+                                                                .age(employee.getAge().get())
+                                                                .gender(employee.getGender().get())
+                                                                .build();
+        Function<Person, Person> fPerson = person -> new Person.PersonBuilder()
+                                                            .name(person.getName().get())
+                                                            .age(person.getAge().get())
+                                                            .build();
+        Tuple2<Employee, Person> tuple2After = tuple2Before.map(fEmployee, fPerson);
+
+        assertEquals(expected,tuple2After._1.getSalary());
+
+        assertSame(employeeAngelo,tuple2Before._1);
+        assertSame(personMarco,tuple2Before._2);
+        assertNotSame(employeeAngelo,tuple2After._1);
+        assertNotSame(personMarco,tuple2After._2);
     }
 }
