1a301c00a74835db11e924e9946728f6657fce12
angelo <magangde@yahoo.de>
2018-05-23 13:10:02 +0200

    Refactoring for Build error

--- OCAExamples/src/main/java/com/maggioni/PartI/Chapter3/Exercise/BirthDays.java
+++ OCAExamples/src/main/java/com/maggioni/PartI/Chapter3/Exercise/BirthDays.java
@@ -14,23 +14,23 @@ public class BirthDays {
                 System.out.println(yearentry);
             }
         }
-        
+        /*
         int[] localYear;
         if (localYear != null) {    // you will get a runtime error, becuase local arrays are not initialized
             for (int m : localYear) {
                 System.out.println(m);
             }
         }
-        
+        */
         for (int monthentry : month) {      // no problem here becase array has been initialized
             System.out.println(monthentry);
         }
-        
+        /*
         Date date;
         if (date == null) {     // you will get a run-time error, because date is not initialized
             System.out.println("date is null");
         }
-
+        */
     }
 
 }
