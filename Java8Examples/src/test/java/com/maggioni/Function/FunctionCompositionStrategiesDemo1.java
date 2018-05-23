395c9d5899c0c404a47b4993c86af8c4325f7fcb
angelo <magangde@yahoo.de>
2018-05-23 08:19:23 +0200

    Fanction Composition Strategies Demo

--- Java8Examples/src/test/java/com/maggioni/Function/FunctionCompositionStrategiesDemo1.java
+++ Java8Examples/src/test/java/com/maggioni/Function/FunctionCompositionStrategiesDemo1.java
@@ -2,9 +2,7 @@ package com.maggioni.Function;
 
 import org.junit.Test;
 
-import java.util.function.BinaryOperator;
-import java.util.function.Function;
-import java.util.function.UnaryOperator;
+import java.util.function.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,29 +16,35 @@ public class FunctionCompositionStrategiesDemo1 {
     private UnaryOperator<Integer> mult3Unary = y -> y * 3;
 
     private BinaryOperator<Function<Integer, Integer>> functionBinaryOperator = (sum, mult) -> x -> sum.apply(mult.apply(x));
-    Integer result = 5 * 3 + 3;
+    private Integer result = 5 * 3 + 3;
+
+    private Supplier assertResultIsOK = () -> assertThat(compose.apply(5)).isEqualTo(result);
 
     @Test
     public void givenTwoFunctions_whenComposingWithBinaryFunction_thenResultIsOK() {
         compose = functionBinaryOperator.apply(sum1, mult3);
 
-        assertThat(compose.apply(5)).isEqualTo(result);
+        assertResultIsOK.get();
     }
 
     @Test
     public void givenTwoFunctions_whenComposingWithCompose_thenResultIsOK() {
         compose = sum1.compose(mult3);
 
-        assertThat(compose.apply(5)).isEqualTo(result);
+        assertResultIsOK.get();
     }
 
     @Test
     public void givenTwoUnaryOperators_whenComposingWithBinary_thenResultIsOK() {
         compose = functionBinaryOperator.apply(sum1Unary, mult3Unary);
 
-        assertThat(compose.apply(5)).isEqualTo(result);
+        assertResultIsOK.get();
+    }
 
+    @Test
+    public void givenTwoUnaryOperators_whenComposing_thenResultIsOK() {
         compose = sum1Unary.compose(mult3Unary);
-        assertThat(compose.apply(5)).isEqualTo(result);
+
+        assertResultIsOK.get();
     }
 }
