386da3c773e63a307d8f8bff8330b7ac0c2d6de2
Angelo <Angelo@DESKTOP-PQRPD3V.fritz.box>
2018-05-25 13:39:34 +0200

    OptionDemo1 added / Person / Address / City Entities added

--- Vavr/src/test/java/TryAnOptionalMonads/c_Demos/OptionDemo1.java
+++ Vavr/src/test/java/TryAnOptionalMonads/c_Demos/OptionDemo1.java
@@ -31,9 +31,9 @@ public class OptionDemo1 {
 
     HashMap<String, Person> personMap = new HashMap<>();
     HashMap<String, Option<Person>> personMapJava7 = new HashMap<>();
-    Map<String,Person> personMapJava8 = new Map<>();
+    Map<String, Person> personMapJava8 = new Map<>();
 
-    BiConsumer<City, City> assertPerson3IsCompleted = (cityToTest, cityResult) -> assertThat(cityToTest).isEqualTo(cityResult);
+    BiConsumer<City, City> assertCity = (cityToTest, cityResult) -> assertThat(cityToTest).isEqualTo(cityResult);
 
     @Before
     public void setUp() {
@@ -45,14 +45,14 @@ public class OptionDemo1 {
         personMapJava7.put(namePaolo, Option.of(person2));
         personMapJava7.put(nameDaniela, Option.of(person3));
 
-        personMapJava8.put(nameMarco,person1);
-        personMapJava8.put(namePaolo,person2);
-        personMapJava8.put(nameDaniela,person3);
+        personMapJava8.put(nameMarco, person1);
+        personMapJava8.put(namePaolo, person2);
+        personMapJava8.put(nameDaniela, person3);
     }
 
 
     @Test
-    public void givenAPersonMap_whenCheckNullObjectJava7_thenShouldPerson3() {
+    public void givenAPersonMap_whenCheckNullObjectJava7_thenShouldCityMailand() {
         for (String name : personMap.keySet()) {
             Person person = personMap.get(name);
             if (person != null) {
@@ -60,7 +60,7 @@ public class OptionDemo1 {
                 if (address != null) {
                     City city = address.getCity();
                     if (city != null) {
-                        assertPerson3IsCompleted.accept(city, cityMailand);
+                        assertCity.accept(city, cityMailand);
                     }
                 }
             }
@@ -68,7 +68,7 @@ public class OptionDemo1 {
     }
 
     @Test
-    public void givenAPersonMap_whenCheckNullObjectWithOptionJava7Style__thenShouldPerson3() {
+    public void givenAPersonMap_whenCheckNullObjectWithOptionJava7Style_thenShouldCityMailand() {
         for (String name : personMapJava7.keySet()) {
             Option<Person> person = personMapJava7.get(name);
             if (person.isDefined()) {
@@ -76,7 +76,7 @@ public class OptionDemo1 {
                 if (adress.isDefined()) {
                     Option<City> city = adress.get().getCityOption();
                     if (city.isDefined()) {
-                        assertPerson3IsCompleted.accept(city.get(), cityMailand);
+                        assertCity.accept(city.get(), cityMailand);
                     }
                 }
             }
@@ -85,16 +85,16 @@ public class OptionDemo1 {
 
 
     @Test
-    public void givenAPersonMap_whenCheckNullObjectWithOptionJava8Style__thenShouldPerson3() {
+    public void givenAPersonMap_whenCheckNullObjectWithOptionJava8Style_thenShouldCityMailand() {
         for (String name : personMapJava8.keySet()) {
-                personMapJava8.find(name)
-                        .flatMap(Person::getAdressOption)
-                        .flatMap(Address::getCityOption)
-                        .forEach( (city) ->assertPerson3IsCompleted.accept(city, cityMailand));
+            personMapJava8.find(name)
+                    .flatMap(Person::getAdressOption)
+                    .flatMap(Address::getCityOption)
+                    .forEach((city) -> assertCity.accept(city, cityMailand));
         }
     }
 
-    public static class Map<T, U> extends HashMap<T,U> {
+    public static class Map<T, U> extends HashMap<T, U> {
 
         public Option<U> find(T t) {
             return Option.of(super.get(t));
