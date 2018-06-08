ef17b228c8c54819213e7a23d17c5b85e027ba81
Angelo <Angelo@DESKTOP-PQRPD3V.fritz.box>
2018-06-08 15:36:05 +0200

    Test with Functions - 2

--- Vavr/src/test/java/FunctionalProgrammingInJavaBook/MyFunctionOfFunctionsTest.java
+++ Vavr/src/test/java/FunctionalProgrammingInJavaBook/MyFunctionOfFunctionsTest.java
@@ -46,9 +46,64 @@ public class MyFunctionOfFunctionsTest {
         };
     }
 
+    static MyFunction<MyFunction<Integer, String>,
+            MyFunction<String, String>> mango() {
+        return arg1 -> arg2 -> arg1 + arg2;
+    }
+
+    static MyFunction<MyFunction<Integer,Integer>,
+                            MyFunction<MyFunction<Integer,Integer>,
+                                            MyFunction<Integer,Integer>>> comisch() {
+        return new MyFunction<MyFunction<Integer, Integer>, MyFunction<MyFunction<Integer, Integer>, MyFunction<Integer, Integer>>>() {
+            @Override
+            public MyFunction<MyFunction<Integer, Integer>, MyFunction<Integer, Integer>> apply(MyFunction<Integer, Integer> arg1) {
+                return new MyFunction<MyFunction<Integer, Integer>, MyFunction<Integer, Integer>>() {
+                    @Override
+                    public MyFunction<Integer, Integer> apply(MyFunction<Integer, Integer> arg2) {
+                        return new MyFunction<Integer, Integer>() {
+                            @Override
+                            public Integer apply(Integer arg3) {
+                                return arg1.apply(5) + arg2.apply(6) + arg3;
+                            }
+                        };
+                    }
+                };
+            }
+        };
+    }
+
+    static MyFunction<MyFunction<Integer,String>,
+                            MyFunction<MyFunction<Integer,Integer>,
+                                            MyFunction<Integer,Integer>>> comischLa() {
+        return new MyFunction<MyFunction<Integer, String>, MyFunction<MyFunction<Integer, Integer>, MyFunction<Integer, Integer>>>() {
+            @Override
+            public MyFunction<MyFunction<Integer, Integer>, MyFunction<Integer, Integer>> apply(MyFunction<Integer, String> arg1) {
+                return new MyFunction<MyFunction<Integer, Integer>, MyFunction<Integer, Integer>>() {
+                    @Override
+                    public MyFunction<Integer, Integer> apply(MyFunction<Integer, Integer> arg2) {
+                        return new MyFunction<Integer, Integer>() {
+                            @Override
+                            public Integer apply(Integer arg3) {
+                                return  Integer.valueOf(arg1.apply(4)) + arg2.apply(5) + arg3;
+                            }
+                        };
+                    }
+                };
+            }
+        };
+    }
+
+    MyFunction<Integer, String> a = integer -> Integer.toString(integer);
+    MyFunction<Integer, Integer> b = integer -> integer * 2;
+
     @Test
     public void given_when_then() {
         String hello = hello().apply("hello");
         assertThat(hello).isEqualToIgnoringCase("Hello");
+    }
+
+    @Test
+    public void name() {
+        Integer result = comischLa().apply(a).apply(b).apply(3);
     }
 }
