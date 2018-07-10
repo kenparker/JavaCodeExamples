72ad514f205326ff75916caec17547e9f1d840d5
angelo <magangde@yahoo.de>
2018-07-10 15:09:59 +0200

    Chapter 5 Sum and Product Stack Based

---
+++ Vavr/src/test/java/FunctionalProgrammingInJavaBook_Chapter_5/Exercise_5_7/ProductStackBase.java
@@ -0,0 +1,13 @@
+package FunctionalProgrammingInJavaBook_Chapter_5.Exercise_5_7;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ProductStackBase {
+
+    @Test
+    public void givenADoubleList_whenProduct_then() {
+
+    }
+}
