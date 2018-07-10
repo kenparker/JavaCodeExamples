94465bda462e3b51ce8abc0d7dd8823eb7636f06
angelo <magangde@yahoo.de>
2018-07-10 10:59:30 +0200

    Chapter 5 Exercises 5.6

--- Vavr/src/test/java/FunctionalProgrammingInJavaBook_Chapter_5/Exercise_5_3_5_4/ReverseTest.java
+++ Vavr/src/test/java/FunctionalProgrammingInJavaBook_Chapter_5/Exercise_5_3_5_4/ReverseTest.java
@@ -6,10 +6,11 @@ import org.junit.Test;
 import static FunctionalProgrammingInJavaBook_Chapter_5.Common.List.*;
 import static FunctionalProgrammingInJavaBook_Chapter_5.Common.List.list;
 import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.ThrowableAssert.*;
 
 public class ReverseTest {
 
-    List<String> list1 = list(" ", "T", "h","i","s"," ");
+    List<String> list1 = list(" ", "T", "h", "i", "s", " ");
 
     @Test
     public void givenAList_whenReverse_then() {
@@ -27,5 +28,19 @@ public class ReverseTest {
         System.out.println(reverse3);
         assertThat(reverse3.toString()).isEqualTo("[ , T, h, i, s, NIL]");
 
+    }
+
+    @Test
+    public void givenAnEmptyList_whenReverse_thenThisIsReturned() {
+        List<String> list = list();
+        List<String> reverse = list.reverse();
+        assertThat(reverse).isEqualTo(list);
+
+    }
+
+    @Test
+    public void givenAnEmptyList_whenInit_thenIllegalStateException() {
+        ThrowingCallable throwingCallable = () -> init(list());
+        assertThatIllegalStateException().isThrownBy(throwingCallable);
     }
 }
