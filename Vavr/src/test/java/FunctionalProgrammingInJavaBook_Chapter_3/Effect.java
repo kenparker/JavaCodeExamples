65a5fea9c303d6955c8cbc885b547d3b1f175025
angelo <magangde@yahoo.de>
2018-07-31 14:22:44 +0200

    Functional Programming in Java Exercises moved to a new Module / delete old objects

--- Vavr/src/test/java/FunctionalProgrammingInJavaBook_Chapter_3/Effect.java
+++
@@ -1,7 +0,0 @@
-package FunctionalProgrammingInJavaBook_Chapter_3;
-
-public interface Effect<T> {
-
-    public void apply(T t);
-
-}
--- Vavr/src/test/java/FunctionalProgrammingInJavaBook_Chapter_3/Executable.java
+++
@@ -1,5 +0,0 @@
-package FunctionalProgrammingInJavaBook_Chapter_3;
-
-public interface Executable {
-    void exec();
-}
